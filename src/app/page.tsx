234af35cba100f0e74c15165f44a25c2d0e57d45
Chamithu Ruberu <109074596+ChamithuRuberu@users.noreply.github.com>
2024-12-14 23:45:27 +0530

    page updated

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,4 +1,3 @@
-import About from '@/components/about/About'
 import Footer from '@/components/footer/Footer'
 import HeroSection from '@/components/hero-section/HeroSection'
 import Navbar from '@/components/navbar/Navbar'
