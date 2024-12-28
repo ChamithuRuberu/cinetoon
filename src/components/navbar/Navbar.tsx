42600bd843b0893e4087783f097522fcc7907a44
Chamithu Ruberu <109074596+ChamithuRuberu@users.noreply.github.com>
2024-12-28 07:25:44 +0530

    navbar updated

--- src/components/navbar/Navbar.tsx
+++ src/components/navbar/Navbar.tsx
@@ -12,7 +12,7 @@ export default function Navbar() {
   };
 
   return (
-    <nav className="bg-white border-gray-200 dark:bg-gray-900">
+    <nav className=" border-gray-200 bg-gray-900">
       <div className="max-w-screen-xl flex flex-wrap items-center justify-between mx-auto p-4">
         <a href="#" className="flex items-center space-x-3 rtl:space-x-reverse">
           <Image
@@ -20,7 +20,7 @@ export default function Navbar() {
             width={180} // Adjust as per your needs
             height={80} // Adjust as per your needs
             alt="Flowbite Logo"
-          />    
+          />
         </a>
         <button onClick={toggleMenu} data-collapse-toggle="navbar-default" type="button" className="inline-flex items-center p-2 w-10 h-10 justify-center text-sm text-gray-500 rounded-lg md:hidden hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-gray-200 dark:text-gray-400 dark:hover:bg-gray-700 dark:focus:ring-gray-600" aria-controls="navbar-default" aria-expanded="false">
           <span className="sr-only">Open main menu</span>
@@ -29,24 +29,25 @@ export default function Navbar() {
           </svg>
         </button>
         <div className={`${isMenuOpen ? '' : 'hidden'} w-full md:block md:w-auto`}>
-          <ul className="font-medium flex flex-col p-2 md:p-0 mt-4 border border-gray-100 rounded-lg bg-gray-50 md:flex-row md:space-x-8 rtl:space-x-reverse md:mt-0 md:border-0 md:bg-white dark:bg-gray-800 md:dark:bg-gray-900 dark:border-gray-700">
+          <ul className="font-medium flex flex-col p-2 md:p-0 mt-4 border rounded-lg md:flex-row md:space-x-8 rtl:space-x-reverse md:mt-0 md:border-0">
             <li>
-              <a href="#" className="block py-2 px-3 text-white bg-blue-700 rounded md:bg-transparent md:text-blue-700 md:p-0 dark:text-white md:dark:text-blue-500" aria-current="page">Home</a>
+              <a href="#" className="block py-2 px-3 rounded hover:bg-blue-400  hover:text-black md:hover:bg-transparent md:border-0 md:hover:text-blue-400 md:p-0 text-white md:font-semibold">Home</a>
             </li>
             <li>
-              <a href="#" className="block py-2 px-3 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 dark:text-white md:dark:hover:text-blue-500 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent">Services</a>
+              <a href="#" className="block py-2 px-3 rounded hover:bg-blue-400  hover:text-black md:hover:bg-transparent md:border-0 md:hover:text-blue-400 md:p-0 text-white md:font-semibold">Services</a>
             </li>
             <li>
-              <a href="#" className="block py-2 px-3 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 dark:text-white md:dark:hover:text-blue-500 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent">About</a>
+              <a href="#" className="block py-2 px-3 rounded hover:bg-blue-400  hover:text-black md:hover:bg-transparent md:border-0 md:hover:text-blue-400 md:p-0 text-white md:font-semibold">About</a>
             </li>
             <li>
-              <a href="#" className="block py-2 px-3 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 dark:text-white md:dark:hover:text-blue-500 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent">Testimonials</a>
+              <a href="#" className="block py-2 px-3 rounded hover:bg-blue-400 hover:text-black md:hover:bg-transparent md:border-0 md:hover:text-blue-400 md:p-0 text-white md:font-semibold">Testimonials</a>
             </li>
             <li>
-              <a href="#" className="block py-2 px-3 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 dark:text-white md:dark:hover:text-blue-500 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent">Contact</a>
+              <a href="#" className="block py-2 px-3 rounded hover:bg-blue-400  hover:text-black md:hover:bg-transparent md:border-0 md:hover:text-blue-400 md:p-0 text-white md:font-semibold">Contact</a>
             </li>
           </ul>
         </div>
+
       </div>
     </nav>
 
