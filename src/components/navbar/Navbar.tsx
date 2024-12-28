24dff94bdd97e3a5a56da15b826059bf36e115d5
Chamithu Ruberu <109074596+ChamithuRuberu@users.noreply.github.com>
2024-12-28 18:52:10 +0530

    issue fixed

--- src/components/navbar/Navbar.tsx
+++ src/components/navbar/Navbar.tsx
@@ -1,5 +1,6 @@
 "use client"
 import Image from 'next/image';
+import Link from 'next/link';
 import React, { useState } from 'react'
 
 export default function Navbar() {
@@ -14,14 +15,14 @@ export default function Navbar() {
   return (
     <nav className=" border-gray-200 bg-gray-900">
       <div className="max-w-screen-xl flex flex-wrap items-center justify-between mx-auto p-4">
-        <a href="#" className="flex items-center space-x-3 rtl:space-x-reverse">
+        <Link href="#" className="flex items-center space-x-3 rtl:space-x-reverse">
           <Image
             src="/logo.png"
             width={180} // Adjust as per your needs
             height={80} // Adjust as per your needs
             alt="Flowbite Logo"
           />
-        </a>
+        </Link>
         <button onClick={toggleMenu} data-collapse-toggle="navbar-default" type="button" className="inline-flex items-center p-2 w-10 h-10 justify-center text-sm text-gray-500 rounded-lg md:hidden hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-gray-200 dark:text-gray-400 dark:hover:bg-gray-700 dark:focus:ring-gray-600" aria-controls="navbar-default" aria-expanded="false">
           <span className="sr-only">Open main menu</span>
           <svg className="w-5 h-5" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 17 14">
@@ -31,19 +32,19 @@ export default function Navbar() {
         <div className={`${isMenuOpen ? '' : 'hidden'} w-full md:block md:w-auto`}>
           <ul className="font-medium flex flex-col p-2 md:p-0 mt-4 border rounded-lg md:flex-row md:space-x-8 rtl:space-x-reverse md:mt-0 md:border-0">
             <li>
-              <a href="/" className="block py-2 px-3 rounded hover:bg-blue-400  hover:text-black md:hover:bg-transparent md:border-0 md:hover:text-blue-400 md:p-0 text-white md:font-semibold">Home</a>
+              <Link href="/" className="block py-2 px-3 rounded hover:bg-blue-400  hover:text-black md:hover:bg-transparent md:border-0 md:hover:text-blue-400 md:p-0 text-white md:font-semibold">Home</Link>
             </li>
             <li>
-              <a href="#" className="block py-2 px-3 rounded hover:bg-blue-400  hover:text-black md:hover:bg-transparent md:border-0 md:hover:text-blue-400 md:p-0 text-white md:font-semibold">Services</a>
+              <Link href="#" className="block py-2 px-3 rounded hover:bg-blue-400  hover:text-black md:hover:bg-transparent md:border-0 md:hover:text-blue-400 md:p-0 text-white md:font-semibold">Services</Link>
             </li>
             <li>
-              <a href="#" className="block py-2 px-3 rounded hover:bg-blue-400  hover:text-black md:hover:bg-transparent md:border-0 md:hover:text-blue-400 md:p-0 text-white md:font-semibold">About</a>
+              <Link href="#" className="block py-2 px-3 rounded hover:bg-blue-400  hover:text-black md:hover:bg-transparent md:border-0 md:hover:text-blue-400 md:p-0 text-white md:font-semibold">About</Link>
             </li>
             <li>
-              <a href="#" className="block py-2 px-3 rounded hover:bg-blue-400 hover:text-black md:hover:bg-transparent md:border-0 md:hover:text-blue-400 md:p-0 text-white md:font-semibold">Testimonials</a>
+              <Link href="#" className="block py-2 px-3 rounded hover:bg-blue-400 hover:text-black md:hover:bg-transparent md:border-0 md:hover:text-blue-400 md:p-0 text-white md:font-semibold">Testimonials</Link>
             </li>
             <li>
-              <a href="#" className="block py-2 px-3 rounded hover:bg-blue-400  hover:text-black md:hover:bg-transparent md:border-0 md:hover:text-blue-400 md:p-0 text-white md:font-semibold">Contact</a>
+              <Link href="#" className="block py-2 px-3 rounded hover:bg-blue-400  hover:text-black md:hover:bg-transparent md:border-0 md:hover:text-blue-400 md:p-0 text-white md:font-semibold">Contact</Link>
             </li>
           </ul>
         </div>
