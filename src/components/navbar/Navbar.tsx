7d2ba343b0dffb45fe4cfe6a496d1659dfaf2c34
Chamithu Ruberu <109074596+ChamithuRuberu@users.noreply.github.com>
2024-12-29 10:15:41 +0530

    graphic design tab updated

--- src/components/navbar/Navbar.tsx
+++ src/components/navbar/Navbar.tsx
@@ -15,7 +15,7 @@ export default function Navbar() {
   return (
     <nav className=" border-gray-200 bg-gray-900">
       <div className="max-w-screen-xl flex flex-wrap items-center justify-between mx-auto p-4">
-        <Link href="#" className="flex items-center space-x-3 rtl:space-x-reverse">
+        <Link href="/" className="flex items-center space-x-3 rtl:space-x-reverse">
           <Image
             src="/logo.png"
             width={180} // Adjust as per your needs
